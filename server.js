7bedeb8084e848a14b8212ebecc34c68d0c42209
Amey Prakash Tari <116430042+AmeyTari2002@users.noreply.github.com>
2024-04-19 21:33:41 +0530

    Update server.js

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ app.use(morgan('dev'))
 app.use(cors(
     {
         Credential: true,
-        origin: "http://localhost:5173"
+        origin: "https://ameytari-plant-nursery-app.vercel.app"
     }
 ))
 
